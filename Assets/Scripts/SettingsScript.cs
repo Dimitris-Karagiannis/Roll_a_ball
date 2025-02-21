03fdc0d7c26e2846d5e51018c5ed2c9fceaa8399
Dimitris Karagiannis <jim.caso.003@gmail.com>
2025-02-21 17:23:38 +0200

    Added dropdown settings

--- Assets/Scripts/SettingsScript.cs
+++ Assets/Scripts/SettingsScript.cs
@@ -1,47 +1,53 @@
 using UnityEngine;
-using UnityEngine.UI; // Import UI for Toggle component
-using TMPro;
+using UnityEngine.UI;
+using TMPro; // Import TextMeshPro for TMP_Dropdown
 
 public class SettingsScript : MonoBehaviour
 {
-
     public Slider accelerometer_slider;
     public Slider gyroscope_slider;
 
-    public Toggle accelerometer_toggle;
-    public Toggle gyroscope_toggle;
+    public TMP_Dropdown controlDropdown; // Dropdown for selecting control mode
 
     public TextMeshProUGUI accelerometer_speed_value_text;
     public TextMeshProUGUI gyroscope_speed_value_text;
 
     void Start()
     {
-        // Add listeners to detect value change
+        // Add listeners to detect value change in sliders
         accelerometer_slider.onValueChanged.AddListener(value => UpdateValueText(value, accelerometer_speed_value_text));
         gyroscope_slider.onValueChanged.AddListener(value => UpdateValueText(value, gyroscope_speed_value_text));
 
-        // Add listeners for toggles to switch modes
-        accelerometer_toggle.onValueChanged.AddListener(isOn => { if (isOn) ActivateAccelerometerOnly(); });
-        gyroscope_toggle.onValueChanged.AddListener(isOn => { if (isOn) ActivateGyroscopeOnly(); });
+        // Add listener for dropdown selection
+        controlDropdown.onValueChanged.AddListener(OnDropdownValueChanged);
+
+        // Ensure a valid option is selected at start
+        OnDropdownValueChanged(controlDropdown.value);
+    }
+
+    private void OnDropdownValueChanged(int index)
+    {
+        // Assume dropdown options are: 0 = Accelerometer, 1 = Gyroscope
+        if (index == 0)
+        {
+            ActivateAccelerometerOnly();
+        }
+        else if (index == 1)
+        {
+            ActivateGyroscopeOnly();
+        }
     }
 
     public void ActivateGyroscopeOnly()
     {
         gyroscope_slider.gameObject.SetActive(true);
         accelerometer_slider.gameObject.SetActive(false);
-        accelerometer_toggle.isOn = false;
-
-        UpdateValueText(gyroscope_slider.value, gyroscope_speed_value_text);
-
     }
 
     public void ActivateAccelerometerOnly()
     {
         gyroscope_slider.gameObject.SetActive(false);
         accelerometer_slider.gameObject.SetActive(true);
-        gyroscope_toggle.isOn = false;
-
-        UpdateValueText(accelerometer_slider.value, accelerometer_speed_value_text);
     }
 
     public void UpdateValueText(float value, TextMeshProUGUI speed_text)
